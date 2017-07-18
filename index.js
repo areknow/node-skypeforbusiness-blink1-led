828fe35c5c7fc57eea32b9b6fe8d8d4da9b92294
Arnaud Crowther <hi@arnaud.cr>
2017-07-18 16:35:33 -0400

    clean up

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var blink1 = new Blink1();
 var moment = require('moment');
 var timestamp = moment().toISOString();
 
-//this is the logfile from skype for business mac
+//this is the logfile from skype for business
 file = "/Users/arnaudcr/Library/Containers/com.microsoft.SkypeForBusiness/Data/Library/Logs/com.microsoft.SkypeForBusiness/sfbmac.log"
 
 //add the module and start tailing
@@ -18,19 +18,27 @@ ft = require('file-tail').startTailing(file);
 //search for substring each time a new line is caught
 ft.on('line', function(line) {
   if(line.indexOf(statusString('Online')) > -1) {
-    console.log(timestamp+' -> online detected in log')
+    console.log(timestamp+' -> ONLINE detected in log')
     blink1.fadeToRGB(200, 0, 255, 0, 0, null);
   }
   if(line.indexOf(statusString('Busy')) > -1) {
-    console.log(timestamp+' -> busy detected in log')
+    console.log(timestamp+' -> BUSY detected in log')
+    blink1.fadeToRGB(200, 255, 0, 0, 0, null);
+  }
+  if(line.indexOf(statusString('DoNotDisturb')) > -1) {
+    console.log(timestamp+' -> DND detected in log')
     blink1.fadeToRGB(200, 255, 0, 0, 0, null);
   }
   if(line.indexOf(statusString('Away')) > -1) {
-    console.log(timestamp+' -> away detected in log')
+    console.log(timestamp+' -> AWAY detected in log')
+    blink1.fadeToRGB(200, 255, 255, 0, 0, null);
+  }
+  if(line.indexOf(statusString('BeRightBack')) > -1) {
+    console.log(timestamp+' -> BRB detected in log')
     blink1.fadeToRGB(200, 255, 255, 0, 0, null);
   }
   if(line.indexOf(statusString('IdleOnline')) > -1) {
-    console.log(timestamp+' -> idle detected in log')
+    console.log(timestamp+' -> IDLE detected in log')
     blink1.fadeToRGB(200, 255, 255, 0, 0, null);
   }
 });
@@ -39,10 +47,6 @@ ft.on('line', function(line) {
 
 //build the required status string
 function statusString(status) {
-  return '<?xml version="1.0" encoding="utf-8"?>'+
-    '<resource rel="presence" href="/ucwa/v1/applications/411195242550/me/presence" '+
-    'xmlns="http://schemas.microsoft.com/rtc/2012/03/ucwa">'+
-    '<property name="availability">'+status+'</property></resource>';
+  return '<property name="availability">'+status+'</property></resource>';
 }
-
 
